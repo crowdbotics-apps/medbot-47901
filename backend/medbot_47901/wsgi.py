b038b656c378eb905b89ff99393d2611f06905a0
Crowdbotics <team@crowdbotics.com>
2024-05-14 02:51:48 +0000

    Initial commit

---
+++ backend/medbot_47901/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for medbot_47901 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "medbot_47901.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "medbot-47901",
+  url: "https://medbot-47901.botics.co",
+  api: "https://medbot-47901.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
